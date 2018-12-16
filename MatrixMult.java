d390a5e749d4d355f26c125d706f9bab7aca4fef
Sanganu <kppkmsanu@gmail.com>
2018-12-16 03:05:08 -0700

    matmult & setverhttp

---
+++ MatrixMult.java
@@ -0,0 +1,58 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Arrays;
+public class Main
+{
+	public static void main(String[] args) {
+	    int[][] matrix1 = {{1,2,3},{5,6,7},{9,3,4}};
+	    int[][] matrix2 = {{1,13,4},{9,16,17},{12,4,2}};
+	    int[][] result={{0,0,0},{0,0,0},{0,0,0}};
+	    System.out.println("The Matrix printing elements with brackets");
+	    for(int i=0;i<matrix1.length;i++)
+	    {
+	        for(int j=0;j<matrix1[i].length;j++)
+	        {
+	            System.out.println("("+i+","+j+")= "+matrix1[i][j]);
+	        }
+	    }
+	    System.out.println("The First Matrix given:");
+	     for(int i=0;i<matrix1.length;i++)
+	    {
+	        System.out.println("");
+	        for(int j=0;j<matrix1[i].length;j++)
+	        {
+	            System.out.print("("+i+","+j+")= "+matrix1[i][j]);
+	        }
+	    }
+	    System.out.println("\n======================");
+	    System.out.println(Arrays.deepToString(matrix1));
+	    System.out.println("======================");
+	     System.out.println("The Second Matrix given:");
+	     for(int i=0;i<matrix2.length;i++)
+	    {
+	        System.out.println("");
+	        for(int j=0;j<matrix2[i].length;j++)
+	        {
+	            System.out.print("("+i+","+j+")= "+matrix2[i][j]);
+	        }
+	    }
+	    System.out.println("\n======================");
+	    System.out.println(Arrays.deepToString(matrix2));
+	    System.out.println("======================");
+		System.out.println("The Matrix Multiplication");
+		 for(int i=0;i<result.length;i++)
+	    {
+	        System.out.println("");
+	        for(int j=0;j<result[i].length;j++)
+	        {
+	            result[i][j] = (matrix1[i][j]*matrix2[j][i]);
+	            System.out.print(result[i][j]);
+	        }
+	    }
+	}
+}
--- httpserversetup.js
+++ httpserversetup.js
@@ -1,8 +1,14 @@
 let http = require("http");
 let PORT = 8080;
+var fs = require("fs");
+
 var requestHandler = function(request,response){
     //response.end("Request handled and response sent"+request.url);
     var path = request.url;
+    // fs.readFile(__dirname+request.url,function(err,data){
+    //     if(err) throw(err);
+
+    // });
     switch(path){
         case "/":
           return homePage(path,request,response);
@@ -10,6 +16,14 @@ var requestHandler = function(request,response){
           return page1Display(path,request,response);
         case "/page2":
           return page2Display(path,request,response);
+        case "/serverindex":
+          return indexDisplay(path,request,response);
+        case "/htmltohtml":
+          return requestHandle(`${path}.html`,request,response);
+        // case "/2html":
+        //   return re '2html.html?username='+"IDname"
+        default:
+          return defaultPage(path,request,response);
     }
 }
 var server = http.createServer(requestHandler);
@@ -21,7 +35,10 @@ server.listen(PORT,function(){
 function homePage(path,req,res)
 {
     var myHTML = "<html>"+
-                 "<body><h1>Home Page</h1><a href='/page1'>PAge1</a><a href='/page2'>Page2</a>"+
+                 "<body><h1>Home Page</h1><a href='/page1'>Page1</a><br /><a href='/page2'>Page2</a>"+
+                 "<br /><a href='/serverindex'>Index File</a>"+
+                 "<br /><a href='/bn'>Index File</a>"+
+                 "<br /><a href='/htmltohtml'>HtmlToHtml</a>"+
                  "</body></html";
     res.writeHead(200,{"Content-Type":"text/html"});
     res.end(myHTML);
@@ -31,7 +48,9 @@ function page1Display(path,req,res)
 {
     var myHTML = "<html>"+
                 "<body><h2>Page1</h2><br /><a href='/'>Home Page</a>"+
-                "<a href='/page2'>Page 2</a>";
+                "<br /><a href='serverIndex'>Index File</a>"+
+                "<br /><a href='/htmltohtml'>HtmlToHtml</a>"+
+                "<br /><a href='/page2'>Page 2</a>";
     res.writeHead(200,{"Content-Type":"text/html"});
     res.end(myHTML);
 }
@@ -40,8 +59,44 @@ function page2Display(path,req,res)
 {
     var myHTML = "<html>"+
                 "<body><h2>Page2</h2><br /><a href='/'>Home Page</a>"+
-                "<a href='/page1'>Page 1</a>";
+                "<br /><a href='/htmltohtml'>HtmlToHtml</a>"+
+                "<br /><a href='serverIndex'>Index File</a>"+
+                "<br /><a href='/page1'>Page 1</a>";
     res.writeHead(200,{"Content-Type":"text/html"});
     res.end(myHTML);
+}
+
+function indexDisplay(path,req,res)
+{
+    fs.readFile(__dirname+"/bubblesortindex.html",function(err,data){
+        if(err)res.end("Error in fetching data",err);
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.end(data);
+    });
+}
+
+function defaultPage(path,req,res)
+{
+    req.on("data",function(data){
+        fs.readFile(__dirname+"2html.html",function(err,indexfile){
+            if(err)res.end("Error in servning localstorage file!!!",err);
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.end({index:indexfile,params:data});
+        });
+    });
+    var errorhtml = "<html><body>"+
+                    "<body><h5> Error in fetching the page"+path+
+                    "</h5><br/><a href='/'><button>Return to home</button></a></body></html";
+    res.end(errorhtml);
+
+}
+
+function requestHandle(path,req,res)
+{
+    fs.readFile(__dirname+path,function(err,data){
+            if(err)res.end("Error in servning localstorage file!!!",err);
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.end(data);
+        });
 }
 
